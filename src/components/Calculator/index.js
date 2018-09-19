21cbd7668748b248f9457e831b7dc437f9f24fdc
kamienieckik <kamienieckik@gmail.com>
2018-09-19 23:19:31 +0200

    refactor

---
+++ src/components/Calculator/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./Calculator.css";
+
+import Display from "../Display";
+import ControlPanel from "../ControlPanel";
+import Digits from "../Digits";
+import Operators from "../Operators";
+import History from "../History";
+
+class Calculator extends Component {
+  render() {
+    return (
+      <main className="react-calculator">
+        <Display />
+        <ControlPanel />
+        <Digits />
+        <Operators />
+        <History />
+      </main>
+    );
+  }
+}
+
+export default Calculator;
